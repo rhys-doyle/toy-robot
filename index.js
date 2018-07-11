098ff285e98ca4e6e0ac45b644ddbd5bcb7e298c
Rhys Doyle <rhys.doyle@gmail.com>
2018-07-11 21:00:43 +1000

    place function

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ const topValues = ['80%', '60%', '40%', '20%', '0%'];
 const orientation = [180, 270, 360, 90, 0];
 let currentAngle = undefined;
 const compass = ['North', 'East', 'South', 'West'];
+
+const place = ({x, y, f}) => {
+  if (x < 0 || x > 5 || (y < 0 || y > 5)) {
+    console.log("Invalid Place");
+  } else {
+    console.log("x", x);
+    return {
+      x: x,
+      y: y,
+      f: f
+    };
+  }
+};
