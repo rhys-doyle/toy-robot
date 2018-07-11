d941c58334d61e16e96913e68d0c129c3f944bfa
Rhys Doyle <rhys.doyle@gmail.com>
2018-07-11 21:00:27 +1000

    declaring necessary variables

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ let _location = {
   f: undefined
 };
 
+const leftValues = ["0%", "20%", "40%", "60%", "80%"];
+const topValues = ['80%', '60%', '40%', '20%', '0%'];
+const orientation = [180, 270, 360, 90, 0];
+let currentAngle = undefined;
+const compass = ['North', 'East', 'South', 'West'];
