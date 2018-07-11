9ea8c3e3381c28deb4c378ff0c1945e27247749f
Rhys Doyle <rhys.doyle@gmail.com>
2018-07-11 21:01:24 +1000

    move function

--- index.js
+++ index.js
@@ -28,3 +28,35 @@ const inputCheck = input => {
     throw new Error("Input is undefined");
   }
 };
+
+const move = input => {
+  const facing = input.f;
+  let output = input;
+
+  inputCheck(output);
+
+  if (
+    (output.x === 0 && output.f === 3) ||
+    (output.x === 4 && output.f === 1) ||
+    (output.y === 0 && output.f === 2) ||
+    (output.y === 4 && output.f === 0)
+  ) {
+    console.log("Invalid move command");
+    return output;
+  } else {
+    switch (facing) {
+      case 0:
+        output.y = output.y + 1;
+        break;
+      case 1:
+        output.x = output.x + 1;
+        break;
+      case 2:
+        output.y = output.y - 1;
+        break;
+      case 3:
+        output.x = output.x - 1;
+    }
+    return output;
+  }
+};
