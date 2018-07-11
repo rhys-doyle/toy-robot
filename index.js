1c4df5816a06e210b525a2e2ae2f9c475f4a5c51
Rhys Doyle <rhys.doyle@gmail.com>
2018-07-11 20:58:33 +1000

    not required anymore

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const place = require("./place.js");
-const move = require("./move.js");
-const left = require("./left.js");
-const right = require("./right.js");
-const report = require("./report.js");
 
 let location = {
   x: undefined,
